e5711dbe368311e61aeba2b47bb59333068f9edd
Mwiza MBP 2017 <mwiza@agripredict.com>
2018-07-23 01:26:41 +0200

    Added desc

--- test.py
+++ test.py
@@ -3,4 +3,10 @@ import numpy as np
 arr = np.arange(36).reshape(3, 4, 3)
 print (arr)
 
+"""
+Numpy can provide a way to represent data in n-dimensions, the example above shows how numpy can represent data in the shape of 3, 4, 3.
+Inituitively we would think of data represented by images as an array of n-dimensions, for example, if you have 4000 images that have a height of 30 and width of 30, and an RGB channel (3).
+This data would be represented as the following 4000 x 30 x 30 x 3. Note that 3 is R, G, B channel. If the image has RGBA you can use 4 instead.
+"""
+
 
